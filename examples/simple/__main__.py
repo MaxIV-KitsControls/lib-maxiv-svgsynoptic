a0dfd77987256dabb075ef529866388cf5f82c39
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2015-11-01 15:15:41 +0100

    update example

--- examples/simple/__main__.py
+++ examples/simple/__main__.py
@@ -1,3 +1,8 @@
+"""
+A simple example that shows how to use the SVG synoptic widget
+in a stand alone application.
+"""
+
 import os
 
 from taurus.external.qt import Qt
@@ -5,16 +10,23 @@ from taurus.external.qt import Qt
 from svgsynoptic.synopticwidget import SynopticWidget
 
 
+class ExampleSynopticWidget(SynopticWidget):
+
+    "A custom subclass of the synoptic widget."
+
+    def on_click(self, kind, name):
+        # Overriding the click event handler to print information
+        print "click", kind, name
+
+
 def main():
     qapp = Qt.QApplication([])
-
 
     # We need to give the absolute path to the HTML file
     # because our webview is setup to load assets from the
     # svgsynoptic library's path, not from the module's path.
     path = os.path.dirname(__file__)
-    widget = SynopticWidget(os.path.join(path, "example1.html"))
-    # widget.setModel(
+    widget = ExampleSynopticWidget(os.path.join(path, "example1.html"))
 
     widget.show()
     qapp.exec_()
