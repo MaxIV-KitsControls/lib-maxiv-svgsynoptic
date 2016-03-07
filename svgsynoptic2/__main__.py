66a8b3be841224aa7fc84c69ba8567520d718960
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2016-03-07 16:55:36 +0100

    command line options

--- svgsynoptic2/__main__.py
+++ svgsynoptic2/__main__.py
@@ -1,5 +1,7 @@
 """A simple commandline way to test a synoptic file"""
 
+import json
+from optparse import OptionParser
 import os
 from string import Template
 import sys
@@ -11,22 +13,36 @@ from svgsynoptic2.taurussynopticwidget import TaurusSynopticWidget
 
 def main():
 
-    app = TaurusApplication()
+    parser = OptionParser("usage: %prog [options] SVGFILE")
+    parser.add_option("-s", "--size", dest="size",
+                      help="Window size on form WIDTH,HEIGHT", metavar="WINSIZE")
+    parser.add_option("-t", "--title", dest="title",
+                      help="Window title", metavar="WINTITLE")
+    parser.add_option("-z", "--zoomsteps", dest="zoomsteps", metavar="ZOOMSTEPS",
+                      help="Zoom levels, on form ZOOM1,ZOOM2,...", default="1")
+
+    app = TaurusApplication(cmd_line_parser=parser)
+
+    args = app.get_command_line_args()
+    if len(args) != 1:
+        sys.exit("You need to specify the SVG file to load!")
+    svg = args[0]
+    options = app.get_command_line_options()
+
     widget = TaurusSynopticWidget()
 
     # We'd like the synoptic to "select" the relevant item when
     # the user focuses on a panel. Let's connect a handler to
     # the focusChanged signal that does this.
-    def onFocus(old, new):
+    def onfocus(old, new):
         if new and hasattr(new, "window"):
             for device, panel in widget._panels.items():
                 if panel == new.window():
                     widget.select("model", [device])
-
-    app.focusChanged.connect(onFocus)
+    app.focusChanged.connect(onfocus)
 
     # need absolute path to the SVG file
-    svgfile = os.path.abspath(sys.argv[1])
+    svgfile = os.path.abspath(svg)
 
     # since the svg currently needs to be hardcoded in the HTML, we
     # create a temporary HTML file from a static template.
@@ -34,13 +50,19 @@ def main():
     template = os.path.join(path, "web", "template.html")
     with open(template) as f:
         tmpl = Template(f.read())
-    html = tmpl.substitute(path="/web", svgfile=svgfile, config="{}")
+    zoomsteps = [int(z) for z in options.zoomsteps.split(",")]
+    config = {"view": {"zoomSteps": zoomsteps}}
+    html = tmpl.substitute(path="/web", svgfile=svgfile, config=json.dumps(config))
     with NamedTemporaryFile(suffix=".html") as tf:
         tf.write(html)
         tf.flush()
         widget.setModel(tf.name)
+        if options.size:
+            w, h = options.size.split(",")
+            widget.resize(int(w), int(h))
+
+        widget.setWindowTitle(options.title or os.path.basename(svg))
         widget.show()
-        widget.setWindowTitle(sys.argv[1])
         app.exec_()
 
 
