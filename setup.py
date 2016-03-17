f741b92f8ddb78bfb700f9616cff6d989270e319
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2016-03-17 15:40:46 +0100

    v2.1.2

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 
 setup(
     name="python-svgsynoptic2",
-    version="2.1.1",
+    version="2.1.2",
     description="Widget for displaying a SVG synoptic.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
     license="GPLv3",
     url="http://www.maxlab.lu.se",
     packages=['svgsynoptic2'],
+    include_package_data=True,
     package_data={'svgsynoptic2': ['web/js/*.js', 'web/js/libs/*.js', 'web/css/*.css']}
 )
