237360ce5b69466d4424d65ce5d4da663df2b0a8
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2017-10-13 15:16:57 +0200

    bump v3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-svgsynoptic2",
-    version="2.4",
+    version="3.0",
     description="Widget for displaying a SVG synoptic.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
