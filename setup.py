b76439d0707c8550b936cc5dcc9fe4383e533d87
Juliano Murari <juliano.murari@maxiv.lu.se>
2017-10-24 16:15:43 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="python-svgsynoptic2",
-    version="3.0",
+    version="3.1",
     description="Widget for displaying a SVG synoptic.",
     author="Johan Forsberg",
     author_email="johan.forsberg@maxlab.lu.se",
